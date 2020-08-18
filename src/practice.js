0e628e597ba5dd263cfcf59518cb9aee403ca1d2
Lip Cowan <pcowan90@gmail.com>
2020-08-18 11:40:13 -0700

    finished most of drills

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,31 @@
-console.log('hello, practice')
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+});
+
+function searchByProduceName(searchTerm) {
+knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(res => console.log(res))
+};
+
+searchByProduceName('holo');
+
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+
+  knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .limit(productsPerPage)
+      .offset(offset)
+      .then(res => console.log(res));
+};
+
+paginateProducts(2);
