32a9930e636654eba246afeecc910ebd8eb01dc9
Lip Cowan <pcowan90@gmail.com>
2020-08-18 12:35:52 -0700

    completed assignment and drills

---
+++ src/drills.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+});
+
+function getAllTextItems(searchTerm) {
+    knexInstance
+    .select('name')
+    .from('shopping_list')
+    .where('name', 'ilike', `%${searchTerm}%`)
+    .then(res => console.log(res));
+};
+
+getAllTextItems('bur');
+
+function getPaginated(page) {
+    const productsPerPage = 6;
+    const offset = productsPerPage * (page - 1);
+
+    knexInstance
+        .select('name', 'price')
+        .from('shopping_list')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(res => console.log(res));
+};
+
+getPaginated(3);
+
+function getItemsAdded(daysAgo) {
+    knexInstance
+        .select('name','price')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .then(res => console.log(res));
+};
+
+getItemsAdded(3);
+
+function getTotalByCategory() {
+    knexInstance
+        .select('category AS grocery')
+        .sum('price AS total')
+        .from('shopping_list')
+        .groupBy('category')
+        .orderBy([{column: 'total', order: 'ASC'}])
+        .then(res => console.log(res));
+};
+
+getTotalByCategory();
--- src/practice.js
+++ src/practice.js
@@ -6,26 +6,56 @@ const knexInstance = knex({
     connection: process.env.DB_URL
 });
 
-function searchByProduceName(searchTerm) {
-knexInstance
-    .select('product_id', 'name', 'price', 'category')
-    .from('amazong_products')
-    .where('name', 'ILIKE', `%${searchTerm}%`)
-    .then(res => console.log(res))
+// function searchByProduceName(searchTerm) {
+// knexInstance
+//     .select('product_id', 'name', 'price', 'category')
+//     .from('amazong_products')
+//     .where('name', 'ILIKE', `%${searchTerm}%`)
+//     .then(res => console.log(res))
+// };
+
+// searchByProduceName('holo');
+
+// function paginateProducts(page) {
+//   const productsPerPage = 10;
+//   const offset = productsPerPage * (page - 1);
+
+//   knexInstance
+//       .select('product_id', 'name', 'price', 'category')
+//       .from('amazong_products')
+//       .limit(productsPerPage)
+//       .offset(offset)
+//       .then(res => console.log(res));
+// };
+
+// paginateProducts(2);
+
+// function getProductsWithImages() {
+//     knexInstance
+//         .select('product_id', 'name', 'price', 'category', 'image')
+//         .from('amazong_products')
+//         .whereNotNull('image')
+//         .then(res => console.log(res));
+// };
+
+// getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+        .select('video_name', 'region')
+        .count('date_viewed AS views')
+        .where(
+            'date_viewed',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+        )
+        .from('whopipe_video_views')
+        .groupBy('video_name', 'region')
+        .orderBy([
+            {column: 'region', order: 'ASC'},
+            {column: 'views', order:'DESC'}
+        ])
+        .then(res => console.log(res));
 };
 
-searchByProduceName('holo');
-
-function paginateProducts(page) {
-  const productsPerPage = 10;
-  const offset = productsPerPage * (page - 1);
-
-  knexInstance
-      .select('product_id', 'name', 'price', 'category')
-      .from('amazong_products')
-      .limit(productsPerPage)
-      .offset(offset)
-      .then(res => console.log(res));
-};
-
-paginateProducts(2);
+mostPopularVideosForDays(30);
