d6957f4097a5367ad3d85881dc814daae8132b6e
Lip Cowan <pcowan90@gmail.com>
2020-08-19 11:06:00 -0700

    finished readthrough drills

---
+++ src/blogful.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const knex = require('knex');
+const ArticlesService = require('./articles-service');
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+});
+
+ArticlesService.getAllArticles(knexInstance)
+    .then(articles => console.log(articles))
+    .then(() => 
+        ArticlesService.insertArticle(knexInstance, {
+            title: 'New Title',
+            content: 'New Content',
+            date_published: new Date()
+        })
+        )
+    .then(newArticle => {
+        console.log(newArticle)
+        return ArticlesService.updateArticle(
+            knexInstance,
+            newArticle.id,
+            {title: 'updated title'}
+        ).then(() => ArticlesService.getById(knexInstance, newArticle.id))
+    })
+    .then(article => {
+        console.log(article)
+        return ArticlesService.deleteArticle(knexInstance, article.id)
+    })
+    .finally(() => knexInstance.destroy())
